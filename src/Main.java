fa7de36fdd232ab22a9a0aa437e406f7c276b17e
mslancer <wolfnw@yandex.ru>
2021-07-18 19:06:44 +0500

    unitial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5000;
+        int bonusmile = 20;
+        int bonus = ticket / bonusmile;
+
+        System.out.println(bonus);
+    }
+}
