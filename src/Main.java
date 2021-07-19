8c69007daee81bde737367b9f35c9d2766d1a115
mslancer <85640422+mslancer@users.noreply.github.com>
2021-07-19 20:48:22 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int ticket = 5000;
-        int bonusmile = 20;
-        int bonus = ticket / bonusmile;
+        int bonusMile = 20;
+        int bonus = ticket / bonusMile;
 
         System.out.println(bonus);
     }
